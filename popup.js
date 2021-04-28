aeba1373749c6f5069eed04d72aaaa814bf011cf
José <jose1.rodrigues@epitech.eu>
2021-04-28 14:48:56 +0200

    add : consol.log first article

--- popup.js
+++ popup.js
@@ -1,24 +1,39 @@
 // Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+let findPostBtn = document.querySelector(".js-find-post");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageBackgroundColor,
+//   });
+// });
+
+findPostBtn.addEventListener("click", async () => {
+  console.log("tut tut fils de p***")
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
+        target: { tabId: tab.id },
+        function: findPost,
+      });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
+
+function findPost() {
+  let currentIdex = 0;
+  console.log(document.querySelectorAll("article")[currentIdex])
 }
